f6e28fdd71a6d8fd066b92389af8438d19696a2d
Topaz712 <tzam712@gmail.com>
2023-10-14 19:45:14 -0500

    created: necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    EditServerComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
