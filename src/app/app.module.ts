5d9d828c89abdd5649dbc7c44619e0c64fea7d14
Topaz712 <tzam712@gmail.com>
2023-10-14 19:58:27 -0500

    filled with content like Max

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './servers/server/server.component';
@@ -7,6 +8,8 @@ import { ServersComponent } from './servers/servers.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './users/user/user.component';
+import { HomeComponent } from './home/home.component';
+import { ServersService } from './servers/servers.service';
 
 @NgModule({
   declarations: [
@@ -15,12 +18,14 @@ import { UserComponent } from './users/user/user.component';
     ServersComponent,
     EditServerComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
